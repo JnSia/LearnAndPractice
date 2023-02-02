41c5d87fabbaee820964442197c2b5f93e71be73
jnsia <kia54682@gmail.com>
2023-02-02 09:25:42 +0900

    2320202

--- Nodejs/NodeBird-API/app.js
+++ Nodejs/NodeBird-API/app.js
@@ -1,26 +1,23 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
 const express = require('express');
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const morgan = require('morgan');
 const session = require('express-session');
 const nunjucks = require('nunjucks');
-const passport = require('passport')
+const dotenv = require('dotenv');
 
-const pageRouter = require('./routes/page');
-const authRouter = require('./routes/auth')
-const postRouter = require('./routes/post')
-const userRouter = require('./routes/user')
-
-const {sequelize} = require('./models');
-const passportConfig = require('./passport')
+dotenv.config();
+const indexRouter = require('./routes');
+const authRouter = require('./routes/auth');
+const { sequelize } = require('./models');
+const passportConfig = require('./passport');
 
 const app = express();
 
-app.set('port', process.env.PORT || 8001);
+passportConfig();
 
+app.set('port', process.env.PORT || 8002);
 app.set('view engine', 'html');
 nunjucks.configure('views', {
   express: app,
@@ -35,13 +32,10 @@ sequelize.sync({ force: false })
     console.error(err);
   });
 
-passportConfig()
-
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/img', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
   resave: false,
@@ -52,14 +46,11 @@ app.use(session({
     secure: false,
   },
 }));
-
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', pageRouter);
+app.use('/', indexRouter);
 app.use('/auth', authRouter);
-app.use('/post', postRouter);
-app.use('/user', userRouter);
 
 app.use((req, res, next) => {
   const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
@@ -67,11 +58,11 @@ app.use((req, res, next) => {
   next(error);
 });
 
-// 에러 처리 미들웨어 (next 반드시 포함)
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
-  res.status(err.status || 500).render('error');
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 app.listen(app.get('port'), () => {
