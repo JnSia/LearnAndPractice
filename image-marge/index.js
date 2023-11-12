881cb000bf06d553731a22fb28e84e3a624b0f2c
jnsia <kia54682@gmail.com>
2023-11-12 14:57:10 +0900

    fix. prunning

--- image-marge/index.js
+++
@@ -1,9 +0,0 @@
-// const { writeFile } = require('fs');
-
-const mergeImages = require('merge-image');
-const { Canvas, Image } = require('canvas');
-
-mergeImages(['./images/back/jeomoon-back.png', './images/first/first-1.png'], {
-  Canvas: Canvas,
-  Image: Image,
-}).then((b64) => (document.getElementById('b645').src = b64));
